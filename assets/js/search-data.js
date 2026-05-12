2f9a8bec9ba8f9933b1947102356584b4e79abff
wbroscious <126119243+wbroscious@users.noreply.github.com>
2026-05-12 01:44:14 +0000

    Deploying to gh-pages from @ wbroscious/wbroscious.github.io@91083fb9363af8ff5952e46d09145e684b92d10e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -518,14 +518,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%62%72%6F%73%63%69%6F%75%73@%77%69%73%63.%65%64%75", "_blank");
         },
       },{
         id: 'social-rss',
@@ -533,13 +526,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
